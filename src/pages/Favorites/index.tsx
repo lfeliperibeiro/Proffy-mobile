73f13b4ab063bde1796a00248df7214c72285f79
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-08-06 22:22:29 -0300

    Update

---
+++ src/pages/Favorites/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import PageHeader from '../../components/Pageheader';
+
+import styles from './styles'
+
+function Favorites(){
+    return (
+        <View style={styles.container}>
+            <PageHeader title="Meus proffys Favoritos"/>
+        </View>
+        
+    )
+}
+
+export default Favorites;
